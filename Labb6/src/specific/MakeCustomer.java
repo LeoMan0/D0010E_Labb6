a7ea3dcf0f2c4d7592368c8b7f345f2786e9c29d
LeoMan0 <leoman3628@gmail.com>
2024-02-29 08:23:12 +0100

    makeCustomer init and getters

--- Labb6/src/specific/MakeCustomer.java
+++ Labb6/src/specific/MakeCustomer.java
@@ -1,2 +1,37 @@
-package specific;public class MakeCustomer {
+package specific;
+
+public class MakeCustomer {
+
+
+    float time;
+    float arrivalTime, timeToPick, timeToPay, timeMaxWait;
+
+
+    int EventState = 0;
+    boolean inStore;
+
+    public MakeCustomer(float arrivalTime, float timeToPick, float timeToPay, float timeMaxWait) {
+        this.arrivalTime = arrivalTime;
+        this.timeToPick = timeToPick;
+        this.timeToPay = timeToPay;
+        this.timeMaxWait = timeMaxWait;
+    }
+
+    public float getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public float getTimeToPick() {
+        return timeToPick;
+    }
+
+    public float getTimeToPay() {
+        return timeToPay;
+    }
+
+    public float getTimeMaxWait() {
+        return timeMaxWait;
+    }
+
+
 }
