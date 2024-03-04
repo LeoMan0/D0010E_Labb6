2e966632f2adbdd7a8f885251f718566ad43aed3
JAcky <jacphu-7@student.ltu.se>
2024-03-04 22:15:38 +0100

    updated view and state, change if needed

--- Labb6/src/general/State.java
+++ Labb6/src/general/State.java
@@ -1,4 +1,37 @@
+/*
+ * @ Leo Man
+ * @ Leo Vedberg
+ * @ Jacky Phuong
+ * @ Viktor Sundén
+ */
+
 package general;
 
-public class State {
+import java.util.Observer;
+import java.util.Observer;
+
+public class State extends Observable {
+    private boolean stopped;
+    private double timePassed;
+
+    // Check if simulator is stopped
+    public boolean stopped() {
+        return stopped;
+    }
+
+
+    /*
+     * @return time passed
+     */
+    public double timePassed () {
+        return timePassed;
+    }
+
+    /*
+     * @param event notify about the event in use to observer
+     */
+    public void notify(Event event) {
+        setChanged();
+        notifyObservers(event);
+    }
 }
--- Labb6/src/general/View.java
+++ Labb6/src/general/View.java
@@ -1,3 +1,18 @@
+/*
+ * @ Leo Man
+ * @ Leo Vedberg
+ * @ Jacky Phuong
+ * @ Viktor Sundén
+ */
+
 package general;
-public class View {
+
+import java.util.Observer;
+import java.util.Observable;
+
+//  A class for general view
+public class View implements Observer {
+    public void update (Observable o, Object arg) {
+
+    }
 }
