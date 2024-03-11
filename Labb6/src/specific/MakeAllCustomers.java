61774485ac36c1680d1bd2f80e985a0fe382a4e4
LeoMan0 <leoman3628@gmail.com>
2024-03-11 14:38:33 +0100

    Java docs for MakeCustomer and MakeAllCustomers

--- Labb6/src/specific/MakeAllCustomers.java
+++ Labb6/src/specific/MakeAllCustomers.java
@@ -4,13 +4,28 @@ import random.*;
 
 import java.util.ArrayList;
 
+/**
+ * Generates all customers for the store simulation based on exponential distribution
+ * to model inter-arrival times. This class uses the store's lambda value (arrival rate)
+ * and seed for the random number generator to create all the customers
+ * who will arrive at the store.
+ *
+ * @author Leo Man, Jacky Phuong, Leo Vedberg, Viktor Sundén
+ */
+
 
 public class MakeAllCustomers {
 
 
     ArrayList<MakeCustomer> allCustomers = new ArrayList<>();
 
-
+    /**
+     * Constructs and populates the list of all customers arriving at the store
+     * during its operating hours, based on exponential distribution.
+     *
+     * @param storeState The current state of the store, including parameters such
+     *                   as lambda for arrival rate and closing time.
+     */
     public MakeAllCustomers(StoreState storeState) {
 
         double arrivalTimes = 0;
@@ -33,6 +48,12 @@ public class MakeAllCustomers {
             this.allCustomers.add(new MakeCustomer(customerId, arrivalTimeFloat));
         }
     }
+
+    /**
+     * Returns the list of all customers generated for the simulation.
+     *
+     * @return An ArrayList containing all the customers.
+     */
 
     public ArrayList<MakeCustomer> getAllCustomers() {
         return this.allCustomers;
--- Labb6/src/specific/MakeCustomer.java
+++ Labb6/src/specific/MakeCustomer.java
@@ -1,12 +1,24 @@
 package specific;
 
+/**
+ * Represents an individual customer within the store simulation. This class holds
+ * information about the customer's ID and their arrival time to the store.
+ *
+ * @author Leo Man, Jacky Phuong, Leo Vedberg, Viktor Sundén
+ */
+
 public class MakeCustomer {
 
 
     float arrivalTime;
     int customerId;
 
-    boolean inStore;
+    /**
+     * Constructs a new Customer with a specified ID and arrival time.
+     *
+     * @param customerId  The unique identifier for the customer.
+     * @param arrivalTime The time at which the customer arrives at the store.
+     */
 
     public MakeCustomer(int customerId, float arrivalTime) {
         this.customerId = customerId;
@@ -14,11 +26,20 @@ public class MakeCustomer {
 
     }
 
+    /**
+     * Gets the arrival time of the customer.
+     *
+     * @return The arrival time of the customer.
+     */
     public float getArrivalTime() {
         return arrivalTime;
     }
 
-
+    /**
+     * Gets the customer's ID.
+     *
+     * @return The ID of the customer.
+     */
     public int getCustomerId() {
         return customerId;
     }
